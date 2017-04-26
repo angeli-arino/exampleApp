829668dd9f558d15d63d8ebccabb4ea1dcfcf087
Angeli Arino <Abbysmal@Akotosis-MacBook-Pro.local>
2017-04-27 03:00:24 +1200

    AngularFire2App

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyCkCm075rxqYzbFRah00CMt6lk4QbiarOU",
+  authDomain: "uciro-health-and-safety.firebaseapp.com",
+  databaseURL: "https://uciro-health-and-safety.firebaseio.com",
+  storageBucket: "uciro-health-and-safety.appspot.com",
+  messagingSenderId: "740177406991"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
